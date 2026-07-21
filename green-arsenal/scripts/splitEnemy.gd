ab47b8536c827581351c33f45a6a62f2783b6b44
Jack Schaefer <jackschaefer64@gmail.com>
2026-07-21 15:20:59 -0400

    Fixed split enemy push (?)
    Trying to fix split enemy push

---
+++ green-arsenal/scripts/splitEnemy.gd
@@ -0,0 +1,103 @@
+extends CharacterBody3D
+#Split Enemy Script
+
+@export var speed = 2
+@export var maxHealth = 1
+@export var contact_damage = 1
+@export var enemyScene: PackedScene = preload("res://scenes/splitEnemy.tscn")
+
+var generation: int = 0
+@export var maxSplits: int = 1
+var player = null
+#Change maxSplits if you want the enemy to split more
+
+@onready var currHealth: int = maxHealth
+var move_velocity: Vector3 = Vector3.ZERO
+var target_velocity: Vector3 = Vector3.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	currHealth = maxHealth
+	if player == null:
+		find_player()
+
+func find_player():
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size() > 0:
+		player = players[0]
+		return
+	var level = get_tree().current_scene
+	if level:
+		for c in level.get_children():
+			if c.is_in_group("player_package") and player == null:
+				player = c.get_child(0)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		move_velocity += get_gravity() * delta
+	if player != null:
+		var target_pos = player.global_position
+		target_pos.y = global_position.y 
+		var dist = global_position.distance_to(target_pos)
+		if dist > 0.1 and dist < 10.0:
+			look_at(target_pos, Vector3.UP)
+		var direction = (target_pos - global_position).normalized()
+		move_velocity.x = direction.x * speed
+		move_velocity.z = direction.z * speed
+	else:
+		move_velocity.x = 0
+		move_velocity.z = 0
+	velocity = target_velocity + move_velocity
+	move_and_slide()
+	target_velocity = target_velocity.lerp(Vector3.ZERO, delta)
+func take_damage(amount: int) -> void:
+	currHealth -= amount
+	print("Remaining health: ", currHealth)
+	if currHealth <= 0:
+		die()
+
+func take_knockback(amount: Vector3) -> void:
+	target_velocity += amount
+
+func die() -> void:
+	print("Enemy dead")
+	var offset = Vector3(1.5, 0.5, 0)
+	var newSize = scale * 0.6
+	
+	if generation < maxSplits:
+		var level = get_tree().current_scene
+		
+		var spawn1 = enemyScene.instantiate()
+		spawn1.generation = generation + 1
+		
+		spawn1.global_transform.origin = global_transform.origin - offset
+		spawn1.scale = newSize
+		
+		spawn1.move_velocity = Vector3.ZERO
+		spawn1.target_velocity = Vector3.ZERO
+		spawn1.velocity = Vector3.ZERO
+		
+		level.add_child(spawn1) 
+		
+		var spawn2 = enemyScene.instantiate()
+		spawn2.generation = generation + 1
+		
+		spawn2.global_transform.origin = global_transform.origin + offset
+		spawn2.scale = newSize
+		spawn2.move_velocity = Vector3.ZERO
+		spawn2.target_velocity = Vector3.ZERO
+		spawn2.velocity = Vector3.ZERO
+		
+		level.add_child(spawn2)
+		
+	queue_free()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_hurtbox_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		if body.has_method("take_damage"):
+			if !body.iframes:
+				body.take_damage(contact_damage)
+			take_knockback(5.0 * global_basis.z)
