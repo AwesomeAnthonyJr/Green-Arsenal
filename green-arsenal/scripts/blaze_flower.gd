7bf3b1a20e8b50e593980d6e04b6e3c4d9750f7d
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2026-07-25 12:43:51 -0400

    blaze flower fully imported

--- green-arsenal/scripts/blaze_flower.gd
+++ green-arsenal/scripts/blaze_flower.gd
@@ -1,8 +1,11 @@
 extends Plant
-
+@onready var anim = $AnimationPlayer
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		pass
 		#body.heal_1()
 		#destroy_self()
+
+func grow():
+	anim.play("grow")
