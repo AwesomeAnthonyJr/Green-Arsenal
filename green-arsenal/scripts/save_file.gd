ec92673729f3953edb3f135cebf873537cbd210e
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2026-07-26 02:24:52 -0400

    Merge branch 'main' into NewJack, and tweaked all the rooms a little bit and added textures

--- green-arsenal/scripts/save_file.gd
+++ green-arsenal/scripts/save_file.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name SaveFile
 
 @export var seed_types: Array[bool] = [
-	false,
+	true,
 	false,
 	false,
 	false,
@@ -15,14 +15,17 @@ class_name SaveFile
 
 ###this is for like doors and stuff, depending on the rooms
 @export var game_flags: Array[bool] = [
-	false, #forest 1_NEW - strangler gate
-	false, #forest 2 - torch 1
-	false, #forest 2 - torch 2
-	false, #forest 2 - torch 3
-	false, #forest 2 - strangler gate
-	false, #forest 5 - strangler gate
-	false, #underground_entrance - torch 1
-	false, #underground_entrance - torch 2
+	true, #forest 1_NEW - strangler gate
+	true, #forest 2 - torch 1
+	true, #forest 2 - torch 2
+	true, #forest 2 - torch 3
+	true, #forest 2 - strangler gate
+	true, #forest 5 - strangler gate
+	true, #underground_entrance - torch 1
+	true, #underground_entrance - torch 2
+	false, #underground_2 - torch 1
+	false, #underground_2 - torch 2
+	false, #bounce_seed - button
 ]
 ###this is mostly for the map screen to hide certain layers until its time
 @export var farthest_floor: int = 2
