240dcdc3aba63333119abc0565211a8f6f0a8514
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2026-07-27 08:29:38 -0400

    making demo build for playtesters

--- green-arsenal/scripts/save_file.gd
+++ green-arsenal/scripts/save_file.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name SaveFile
 
 @export var seed_types: Array[bool] = [
-	true, #blaze
+	false, #blaze
 	false, #life
 	false, #bounce
 	false, #seeker
@@ -11,27 +11,27 @@ class_name SaveFile
 	false #heavy
 ]
 @export var max_hp: int = 3
-@export var growth_charges: int = 2
+@export var growth_charges: int = 1
 
 ###this is for like doors and stuff, depending on the rooms
 @export var game_flags: Array[bool] = [
-	true, #forest 1_NEW - strangler gate | 0
-	true, #forest 2 - torch 1
-	true, #forest 2 - torch 2
-	true, #forest 2 - torch 3
-	true, #forest 2 - strangler gate
+	false, #forest 1_NEW - strangler gate | 0
+	false, #forest 2 - torch 1
+	false, #forest 2 - torch 2
+	false, #forest 2 - torch 3
+	false, #forest 2 - strangler gate
 	false, #forest 5 - strangler gate | 5
 	false, #underground_entrance - torch 1
 	false, #underground_entrance - torch 2
 	false, #underground_2 - torch 1
 	false, #underground_2 - torch 2
-	true, #bounce_seed - button | 10
+	false, #bounce_seed - button | 10
 	false, #combo_puzzle_1 - strangler gate
 	false, #bounce_seed - strangler gate
-	true, #underground_3 - button
+	false, #underground_3 - button
 	false, #underground_3 - strangler gate
 ]
 ###this is mostly for the map screen to hide certain layers until its time
-@export var farthest_floor: int = 2
+@export var farthest_floor: int = 1
 ###this is to set the player to load in different positions depending on their last save
-@export var load_point: int = -1
+@export var load_point: int = 0
