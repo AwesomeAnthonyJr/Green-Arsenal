4783284982e46b6718a9c6c7b15b48c00c2946f6
Jack Schaefer <jackschaefer64@gmail.com>
2026-08-03 18:32:43 -0400

    Flooded_4 made

--- green-arsenal/scripts/room_loader.gd
+++ green-arsenal/scripts/room_loader.gd
@@ -53,6 +53,7 @@ const room_dict = {
 	20: "res://scenes/rooms/flooded/flooded_1.tscn",
 	21: "res://scenes/rooms/flooded/flooded_2.tscn",
 	22: "res://scenes/rooms/flooded/flooded_3.tscn",
+	23: "res://scenes/rooms/flooded/flooded_4.tscn",
 	99: "res://scenes/rooms/underground/ending_for_sprint3.tscn",
 }
 #must be the inverse of the first!!!
@@ -80,6 +81,7 @@ const reverse_dict = {
 	"res://scenes/rooms/flooded/flooded_1.tscn": 20,
 	"res://scenes/rooms/flooded/flooded_2.tscn": 21,
 	"res://scenes/rooms/flooded/flooded_3.tscn": 22,
+	"res://scenes/rooms/flooded/flooded_4.tscn": 23,
 	"res://scenes/rooms/underground/ending_for_sprint3.tscn": 99,
 }
 
